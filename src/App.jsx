08573d39fb66dc1770b27aee6158eb8c7aa0a707
Aruan Galves Amaral <aruangalves@gmail.com>
2024-06-25 16:41:13 -0300

    useState React Hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [reverse, setReverse] = useState(false);
+  const rev = reverse ? 'reverse' : '';
+
+  const handleClick = () =>{
+    //setReverse(!reverse);
+    //setCount(count + 1);
+    //It could be advantageous to use setReverse and setCount through a callback function as
+    setReverse((reverse) => !reverse);
+    setCount((prevCount) => prevCount + 1);
+  }
 
   return (
     <>
@@ -13,14 +23,12 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className={`logo react ${rev}`} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button type="button" onClick={handleClick}>Logo was reversed {count} time{count === 1 ? '' : 's'}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
