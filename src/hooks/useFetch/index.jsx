01da2c99eaf9395a097f7d56aaf25d4f2c8c074f
Aruan Galves Amaral <aruangalves@gmail.com>
2024-07-12 16:18:12 -0300

    useFetch custom react hook part 2

--- src/hooks/useFetch/index.jsx
+++ src/hooks/useFetch/index.jsx
@@ -1,31 +1,69 @@
 import { useEffect, useRef, useState } from "react";
 
+//this is not an all-encompassing function to compare objects, but suits well enough for our needs
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+}
+
 //Usage: const [result, loading] = useFetch();
 export const useFetch = (url, options) =>{
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [reload, setReload] = useState(false);
+  //The 'options' dependency causes an infinite loop, this is due to the options being a JavaScript object instead of a primitive type. This means an object with similar attributes will never be detected as equal to a previous, similar object. Example: {} === {} returns false. useRef solves the dependency issue that caused an infinite loop on useEffect
   const urlRef = useRef(url);
   const optionsRef = useRef(options);
 
-  //The 'options' dependency causes an infinite loop, this is due to the options being a JavaScript object instead of a primitive type. This means an object with similar attributes will never be detected as equal to a previous, similar object. Example: {} === {} returns false.
+  //Reload if the reference values are outdated
   useEffect(()=>{
+    let shouldReload = false;
+
+    if(!isObjectEqual(url,urlRef.current)){
+      urlRef.current = url;
+      shouldReload = true;
+    }
+    if(!isObjectEqual(options,optionsRef.current)){
+      optionsRef.current = options;
+      shouldReload = true;
+    }
+
+    if(shouldReload){
+      setReload((r) => !r);
+    }
+
+  },[url, options]);
+
+
+  useEffect(()=>{
+    let wait = false;
     setLoading(true);
 
     const fetchData = async () => {
       try {
-        const response = await fetch(url, options);
+        const response = await fetch(urlRef.current, optionsRef.current);
         const jsonResponse = await response.json();
-        setResult(jsonResponse);
-        setLoading(false);
+
+        if(!wait){
+          setResult(jsonResponse);
+          setLoading(false);
+        }
+
       }
       catch(error){
-        setLoading(false);
+        if(!wait){
+          setLoading(false);
+        }
         throw error;
       }
 
     };
     fetchData();
-  },[url, options]);
+
+    //cleanup done to prevent fetching when the page components are dismounted
+    return () => {
+      wait = true;
+    };
+  },[reload]);
 
   return [result, loading];
 };
