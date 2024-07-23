3dd8bb36c9fc2bb32d2aab3dab3dadec14b6033f
Aruan Galves Amaral <aruangalves@gmail.com>
2024-07-23 15:47:59 -0300

    useHistory, useLocation router hooks

--- src/templates/Abc/index.jsx
+++ src/templates/Abc/index.jsx
@@ -1,7 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams, useLocation, useHistory } from "react-router-dom";
 
 export const Abc = () =>{
   const {slug, id} = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  console.log(location);
+
+  console.log(history);
+
+  useEffect(()=>{
+    setTimeout(function(){
+      history.push('/');
+    },3000);
+  },[history]);
 
   return (
     <div>
