3a55db21b0c8b4b8420429117b389fb7f376c110
Aruan Galves Amaral <aruangalves@gmail.com>
2024-07-23 15:56:00 -0300

    Page not found, general route

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,18 @@ import App from './App'
 import './index.css'
 import { Abc } from './templates/Abc'
 import { Menu } from './components/Menu'
+import { Page404 } from './templates/Page404'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Menu />
       <Switch>
-        <Route path="/abc/:slug?/:id?" component={Abc} />
-        <Route path="/" component={App} />
+        <Route path="/abc/:slug/:id" component={Abc} />
+        <Route path="/abc/:slug" component={Abc} />
+        <Route path="/abc" component={Abc} />
+        <Route path="/" component={App} exact />
+        <Route path="*" component={Page404} />
         {/*<App />*/}
       </Switch>
     </BrowserRouter>
---
+++ src/templates/Page404/index.jsx
@@ -0,0 +1,8 @@
+export const Page404 = () =>{
+  return (
+    <div>
+      <h1>ERROR 404 - Page not found</h1>
+      <p>The URL you've typed could not be match with an existing page, please return to home or try a different location.</p>
+    </div>
+  );
+}
